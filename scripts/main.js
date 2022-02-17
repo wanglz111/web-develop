aa3f16e00fc2f66fd2c4f7a8d364695fba5da5f0
lucas <1339696209@qq.com>
2022-02-18 00:38:54 +0800

    A test for learning git

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-// const myHeading = document.querySelector('h1');
-// myHeading.textContent = 'Hello world!';
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
 
-// document.querySelector('html').addEventListener('click', function() {
-//     const myHeading = document.querySelector('h1');
-//     myHeading.textContent = 'Hello world!';
-//   });
+document.querySelector('html').addEventListener('click', function() {
+    const myHeading = document.querySelector('h1');
+    myHeading.textContent = 'Hello world!';
+});
 
 document.querySelector('p').addEventListener('click', () => {
     console.log("hello world!")
@@ -24,13 +24,4 @@ function setUserName() {
         localStorage.setItem('name', myName);
         myHeading.textContent = 'Mozilla is cool, ' + myName;
     }
-}
-
-// TODO:11111;
-
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
 }
